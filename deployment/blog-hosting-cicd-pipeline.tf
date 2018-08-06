821c4b128c43fd8d7dae2d63e085524d87f3bb8a
Kyle Galbraith <kyle.galbraith459@gmail.com>
2018-08-06 09:39:14 -0700

    add Terraform template for static website hosting

---
+++ deployment/blog-hosting-cicd-pipeline.tf
@@ -0,0 +1,89 @@
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_s3_bucket" "blog" {
+  bucket = "${var.blog_url}"
+
+  policy = <<POLICY
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"AddPerm",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::${var.blog_url}/*"]
+    }
+  ]
+}
+POLICY
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
+}
+
+resource "aws_cloudfront_distribution" "blog_distribution" {
+  origin {
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+
+    domain_name = "${aws_s3_bucket.blog.website_endpoint}"
+    origin_id   = "${var.blog_url}"
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+
+    target_origin_id = "${var.blog_url}"
+    min_ttl          = 0
+    default_ttl      = 86400
+    max_ttl          = 31536000
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  aliases = ["${var.blog_url}"]
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "${var.acm_certificate_arn}"
+    ssl_support_method  = "sni-only"
+  }
+}
+
+resource "aws_route53_record" "blog-record" {
+  zone_id = "${var.route53_hosted_zone}"
+  name    = "${var.blog_url}"
+  type    = "A"
+
+  alias = {
+    name                   = "${aws_cloudfront_distribution.blog_distribution.domain_name}"
+    zone_id                = "${aws_cloudfront_distribution.blog_distribution.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
---
+++ deployment/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  description = "The AWS region to deploy into (default: us-west-2)."
+  default     = "us-west-2"
+}
+
+variable "blog_url" {
+  description = "The url for the blog (i.e.: blog.kylegalbraith.com)"
+  default     = "blog.kylegalbraith.com"
+}
+
+variable "acm_certificate_arn" {
+  description = "ARN for the AWS Certificate Manager SSL certificate (see https://blog.kylegalbraith.com/2018/03/02/adding-free-ssl-certificates-to-static-websites-via-aws-certificate-manager/ for details)"
+  default     = ""
+}
+
+variable "route53_hosted_zone" {
+  description = "Route53 hosted zone to create the alias for the blog"
+  default     = ""
+}
