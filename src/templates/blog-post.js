ca56aa6b5e25dd57228e83fcf27b4dfb8d7f2b85
Evan Sparkman <me@evansparkman.com>
2019-01-18 15:18:38 -0500

    This removes the deprecated pathContext in favor of the new pageContext

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,72 +1,93 @@
-import React from 'react';
-import Img from 'gatsby-image';
-import Link from 'gatsby-link';
-import Seo from '../components/Seo';
-import Social from '../components/Social';
-import config from '../config/config';
-import Layout from '../components/layout';
-import { graphql } from 'gatsby';
+import React from "react";
+import Img from "gatsby-image";
+import Link from "gatsby-link";
+import Seo from "../components/Seo";
+import Social from "../components/Social";
+import config from "../config/config";
+import Layout from "../components/layout";
+import { graphql } from "gatsby";
 
 class BlogPostTemplate extends React.Component {
   render() {
-    const post = this.props.data.markdownRemark
-    const { previous, next } = this.props.pathContext
-    const url = 'https://blog.kylegalbraith.com' + this.props.location.pathname;
+    const post = this.props.data.markdownRemark;
+    const { previous, next } = this.props.pageContext;
+    const url = "https://blog.kylegalbraith.com" + this.props.location.pathname;
 
     return (
       <Layout>
         <div className="text-left p-4 bg-grey-lightest shadow-lg">
           <Seo data={post} />
-          {
-            post.frontmatter.cover &&
-            <Img sizes={post.frontmatter.cover.childImageSharp.sizes} alt={post.frontmatter.title} className="w-full" />
-          }
+          {post.frontmatter.cover && (
+            <Img
+              sizes={post.frontmatter.cover.childImageSharp.sizes}
+              alt={post.frontmatter.title}
+              className="w-full"
+            />
+          )}
           <h1 className="text-3xl lg:text-5xl text-indigo-darker font-normal mt-6 mb-2">
             {post.frontmatter.title}
           </h1>
           <p className="block mb-8 pb-4 border-b-2">
-            <span role="img" aria-label="blog post date">📅</span> {post.frontmatter.date} – {config.authorName}
+            <span role="img" aria-label="blog post date">
+              📅
+            </span>{" "}
+            {post.frontmatter.date} – {config.authorName}
           </p>
-          <div className="blog-content" dangerouslySetInnerHTML={{ __html: post.html }} />
+          <div
+            className="blog-content"
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          />
           <div className="mt-16 pt-8 social-content text-center border-t">
-            <p className="font-light">Did you enjoy this post? Share the <span role="img" aria-label="love the post">❤️</span> with others.</p>
+            <p className="font-light">
+              Did you enjoy this post? Share the{" "}
+              <span role="img" aria-label="love the post">
+                ❤️
+              </span>{" "}
+              with others.
+            </p>
             <Social url={url} title={post.frontmatter.title} />
           </div>
 
           <ul
             className="mt-8 border-t-2 pt-4"
             style={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              justifyContent: 'space-between',
-              listStyle: 'none',
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+              listStyle: "none",
               paddingLeft: 0
             }}
           >
             <li>
-              {
-                previous &&
-                <Link to={previous.fields.slug} rel="prev" className="text-indigo-darker hover:text-indigo-lighter">
+              {previous && (
+                <Link
+                  to={previous.fields.slug}
+                  rel="prev"
+                  className="text-indigo-darker hover:text-indigo-lighter"
+                >
                   ← {previous.frontmatter.title}
                 </Link>
-              }
+              )}
             </li>
             <li>
-              {
-                next &&
-                <Link to={next.fields.slug} rel="next" className="text-indigo-darker hover:text-indigo-lighter">
+              {next && (
+                <Link
+                  to={next.fields.slug}
+                  rel="next"
+                  className="text-indigo-darker hover:text-indigo-lighter"
+                >
                   {next.frontmatter.title} →
-              </Link>
-              }
+                </Link>
+              )}
             </li>
           </ul>
         </div>
       </Layout>
-    )
+    );
   }
 }
 
-export default BlogPostTemplate
+export default BlogPostTemplate;
 
 export const pageQuery = graphql`
   query BlogPostBySlug($slug: String!) {
@@ -84,13 +105,13 @@ export const pageQuery = graphql`
         title
         date(formatString: "DD MMMM, YYYY")
         cover {
-          childImageSharp{
+          childImageSharp {
             sizes(maxWidth: 1140, maxHeight: 420) {
-                ...GatsbyImageSharpSizes_withWebp
+              ...GatsbyImageSharpSizes_withWebp
             }
           }
         }
       }
     }
   }
-`
+`;
